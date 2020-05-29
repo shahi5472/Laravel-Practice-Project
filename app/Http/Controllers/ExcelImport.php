15350a737aa7f867f003e940f1c5276bd30ab82e
shahi5472 <s.m.kamalhussain@gmail.com>
2020-05-30 05:18:15 +0600

    task scheduling

--- app/Http/Controllers/ExcelImport.php
+++ app/Http/Controllers/ExcelImport.php
@@ -7,6 +7,7 @@ use App\Customer;
 use App\EmailMarket;
 use App\Events\NewCustomerRegisterEvent;
 use App\Imports\TransactionsImport;
+use App\Jobs\SendEmail;
 use App\Transaction;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -26,9 +27,9 @@ class ExcelImport extends Controller
         //650 original email
         //716 in database
 
-         $transactions = Transaction::whereNotNull('email')->paginate(50);
-         $number = count(Transaction::whereNotNull('email')->get());
-         return view('excel.index', compact('transactions', 'number'));
+        $transactions = Transaction::whereNotNull('email')->paginate(50);
+        $number = count(Transaction::whereNotNull('email')->get());
+        return view('excel.index', compact('transactions', 'number'));
     }
 
     public function create()
@@ -47,19 +48,18 @@ class ExcelImport extends Controller
 
     public function sendEmail()
     {
-//        $transactions = Transaction::whereNotNull('email')->get();
-//
-//        $array = $transactions;
-//        $data = array();
-//        foreach ($array as $value) {
-//            $data[$value->email] = $value->email;
-//        }
-//        return count(array_values($data));
-
-
         //$transactions[] = ['email' => 'nuheljft@gmail.com', 'name' => "Caca"];
         $transactions[] = ['email' => 's.m.kamalhussain@gmail.com', 'name' => "Kamal Hussain"];
-        $transactions[] = ['email' => 's.m.kamalhussain@hotmail.com', 'name' => "Kamal Hussain"];
+        $transactions[] = ['email' => 's.m.kamalhussain@gmail.com', 'name' => "Kamal Hussain"];
+        $transactions[] = ['email' => 's.m.kamalhussain@gmail.com', 'name' => "Kamal Hussain"];
+        $transactions[] = ['email' => 's.m.kamalhussain@gmail.com', 'name' => "Kamal Hussain"];
+        $transactions[] = ['email' => 's.m.kamalhussain@gmail.com', 'name' => "Kamal Hussain"];
+//        $transactions[] = ['email' => 's.m.kamalhussain@hotmail.com', 'name' => "Kamal Hussain"];
+
+        //for ($i = 0; $i < count($transactions); $i++) {
+        // SendEmail::dispatch($transactions[$i]);
+        // }
+
         //$transactions[] = ['email' => 's.m.kamalhussainshahi@outlook.com', 'name' => "Kamal Hussain"];
 //        $transactions[] = ['email' => 'mustaksayed@hotmail.co.uk', 'name' => "Mustak Sayed"];
         //$transactions[] = ['email' => 's.m.musharaf87@gmail.com', 'name' => "S.m. Sadi"];
---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Transaction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $transaction;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Transaction $transaction)
+    {
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        echo "Test Email Send " . $this->transaction->email;
+    }
+}
